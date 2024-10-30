ad301d504c934b97b617c8005cbcf1bafe4936a7
Clark G <136202713+ceejxy@users.noreply.github.com>
2024-10-29 20:19:47 -0400

    added event listeners to the digit buttons and operator buttons

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// math operations
+
 let add = function(...args) {
     if (args.length === 0) return 0;
 
     return args
-    .reduce((total, arg) => total + arg, 0);
+    .reduce((total, arg) => total + arg);
 }
 
 let subtract = function(...args) {
@@ -26,40 +28,87 @@ let divide = function(...args) {
     .reduce((total, arg) => total / arg)
 }
 
+
+// runs the corresponding operator function
 let operate = function(num1, operator, num2) {
 
     if (operator === '+') return add(num1, num2);
-    if (operator === '—') return subtrat(num1, num2);
+    if (operator === '—') return subtract(num1, num2);
     if (operator === '*') return multiply(num1, num2);
     if (operator === '/') return divide(num1, num2);
-
 }
 
-const addButton = document.querySelector('.plus')
-addButton.addEventListener('click', function() {
-    operator = addButton.innerText;
-    num1 = 2
-    num2 = 9
-
-  console.log(operate(num1, operator, num2));
-
-})
-
 const inputDisplay = document.querySelector('.inputDisplay')
-const digit = document.querySelectorAll('.digit') 
+const operatorButton = document.querySelectorAll('.operator');
 
-digit.forEach(digit => {
-    digit.addEventListener('click', function() {
-        inputDisplay.innerText += digit.innerText;
+// gets the operator that is going to be used. Grays out the button to indicate it has been selected
+operatorButton.forEach(operatorButton => {
+    operatorButton.addEventListener('click', function() {
+        operator = operatorButton.innerText;
+        operatorButton.style.backgroundColor = 'gray';
+        inputDisplay.innerText = '';
+        return operator;
     })
 })
 
-const clear = document.querySelector('.clear')
+const digits = document.querySelectorAll('.digit')
 
+// Makes button show on display when clicked
+digits.forEach(digits => {
+    digits.addEventListener('click', function() {
+        number = digits.innerText;
+        inputDisplay.innerText += number;
+        console.log(inputDisplay.innerText);
+    })
+})
+
+
+//clears display
+const clear = document.querySelector('.clear')
 clear.addEventListener('click', function() {
     if (inputDisplay.innerText != '') {
         inputDisplay.innerText = '';
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* what happens when equals is clicked
+equalsButton.addEventListener('click', function() {
+    inputDisplay.innerText += equalsButton.innerText
+    equation = inputDisplay.innerText.split('+')
+    operator = getOperator();
+    num1 = parseInt(equation[0]);
+    num2 = parseInt(equation[1]);
+
+    console.log(operate(num1, operator, num2));
+    console.log(operator);
+    
+})
+
+
+// displays number that is clicked
+const display = document.querySelectorAll('.display')
+
+display.forEach(display => {
+    display.addEventListener('click', function() {
+        inputDisplay.innerText += display.innerText;
+        //console.log(inputDisplay.innerText)
+    })
+}
+)
+
+*/
 
 
